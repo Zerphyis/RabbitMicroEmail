4b1b0d6ac32fd5800b792155c73a78d9426a9d3d
Otavio <estudosprogotavio@gmail.com>
2025-09-30 03:46:42 -0300

    TASK02 feat:adicionando Classe SmtpEmailSender

---
+++ src/main/java/dev/Zerphyis/MicroRabbitEmail/Infra/config/email/SmtpEmailSender.java
@@ -0,0 +1,24 @@
+package dev.Zerphyis.MicroRabbitEmail.Infra.config.email;
+
+import dev.Zerphyis.MicroRabbitEmail.Domain.model.Email;
+import org.springframework.mail.SimpleMailMessage;
+
+public class SmtpEmailSender implements EmailService{
+    private final JavaMailSenderAdapter javaMailSenderAdapter;
+
+
+    public SmtpEmailSender(JavaMailSenderAdapter javaMailSenderAdapter) {
+        this.javaMailSenderAdapter = javaMailSenderAdapter;
+    }
+
+    @Override
+    public void sendEmail(Email email) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(email.getFromAddres());
+        message.setTo(email.getToAddres());
+        message.setSubject(email.getSubect());
+        message.setText(email.getBody());
+
+        javaMailSenderAdapter.send(message);
+    }
+}
