523d6660307231eac13d509e8608e7385efdb53e
Otavio <estudosprogotavio@gmail.com>
2025-09-30 03:42:42 -0300

    commit inicial

---
+++ src/main/java/dev/Zerphyis/MicroRabbitEmail/MicroRabbitEmailApplication.java
@@ -0,0 +1,13 @@
+package dev.Zerphyis.MicroRabbitEmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroRabbitEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroRabbitEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/Zerphyis/MicroRabbitEmail/MicroRabbitEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Zerphyis.MicroRabbitEmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroRabbitEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
