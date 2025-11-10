51b03ce16b56ffaa360aef51241770a4a4c08577
Otavio <estudosprogotavio@gmail.com>
2025-11-10 03:20:24 -0300

    TASK04 refactor:trocando pasta de EmailRepositoryJpa

--- src/main/java/dev/Zerphyis/MicroRabbitEmail/Domain/repositorys/jpa/EmailRepositoryJpa.java
+++ src/main/java/dev/Zerphyis/MicroRabbitEmail/Domain/repositorys/jpa/EmailRepositoryJpa.java
@@ -1,10 +1,10 @@
 package dev.Zerphyis.MicroRabbitEmail.Domain.repositorys.jpa;
 
-import dev.Zerphyis.MicroRabbitEmail.Domain.model.Email;
+import dev.Zerphyis.MicroRabbitEmail.Infra.coreEntity.EmailEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface EmailRepositoryJpa extends JpaRepository<Email,Long> {
-    List<Email> findBySentFalse();
+public interface EmailRepositoryJpa extends JpaRepository<EmailEntity,Long> {
+    List<EmailEntity> findBySentFalse();
 }
