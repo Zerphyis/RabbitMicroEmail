00ebc491951ff0401dbd899b9ab327390b60e9fa
Otavio <estudosprogotavio@gmail.com>
2025-11-10 03:19:20 -0300

    TASK04 refactor:trocando pasta de EmailRepository

--- src/main/java/dev/Zerphyis/MicroRabbitEmail/Domain/repositorys/EmailRepository.java
+++ src/main/java/dev/Zerphyis/MicroRabbitEmail/Domain/repositorys/EmailRepository.java
@@ -1,13 +1,14 @@
     package dev.Zerphyis.MicroRabbitEmail.Domain.repositorys;
 
     import dev.Zerphyis.MicroRabbitEmail.Domain.model.Email;
+    import dev.Zerphyis.MicroRabbitEmail.Infra.coreEntity.EmailEntity;
 
     import java.util.List;
     import java.util.Optional;
 
     public interface EmailRepository {
         Email save(Email email);
-        Optional<Email> findByid(Long id);
+        Optional<Email> findById(Long id);
         List<Email> findAll();
         void delete(Long id);
         List<Email> findPendingEmails();
