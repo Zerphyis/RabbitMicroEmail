7875192797b268388627db85dd4b0ebcb515cb0b
Otavio <estudosprogotavio@gmail.com>
2025-11-19 13:27:36 -0300

    TASK04 refactor:ajustando pacote de repositoryImpl em EmailRepository

--- src/main/java/dev/Zerphyis/MicroRabbitEmail/Domain/repositorys/EmailRepository.java
+++ src/main/java/dev/Zerphyis/MicroRabbitEmail/Domain/repositorys/EmailRepository.java
@@ -1,7 +1,6 @@
     package dev.Zerphyis.MicroRabbitEmail.Domain.repositorys;
 
     import dev.Zerphyis.MicroRabbitEmail.Domain.model.Email;
-    import dev.Zerphyis.MicroRabbitEmail.Infra.coreEntity.EmailEntity;
 
     import java.util.List;
     import java.util.Optional;
